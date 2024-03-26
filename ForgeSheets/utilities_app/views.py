ca572ff40775b6d53a70464529923fd757806635
Paulo Rosado <paulohenrique110803@gmail.com>
2024-03-26 20:56:16 -0300

    refactoring view register to sign

--- ForgeSheets/utilities_app/views.py
+++ ForgeSheets/utilities_app/views.py
@@ -9,29 +9,19 @@ def sign(request):
     if request.method == "POST":
         if 'login' in request.POST: 
             return login(request, request.POST.get('user'), request.POST.get('password'))
-        elif 'cadastro' in request.POST:
-            return HttpResponse('cadastro')
+        elif 'register' in request.POST:
+            return register(request, request.POST.get('user'), request.POST.get('email'), request.POST.get('password'))
     else:
         return render(request, 'sign.html')
 
 
-def cadastro(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        user = User.objects.filter(username=username).first()
-
-        if user:
-            return HttpResponse('Já existe um usuário com esse username')
-        
-        user = User.objects.create_user(username=username, email=email, password=password)
-        user.save
-        cadastrado = True
-        return redirect('home')
-    else:
-        return render(request, 'cadastro.html')
+def register(request, username, email, password):
+    user = User.objects.filter(username=username).first()
+    if user:
+        return HttpResponse('Já existe um usuário com esse username')
+    user = User.objects.create_user(username=username, email=email, password=password)
+    user.save()
+    return HttpResponse('It runs 👍')
 
 def login(request, username, password):
     print(username, password)
