d6d3b6107e91623e39b98a8b6f41c8f9307296ef
n3waz <lhk@cesar.school>
2024-03-26 17:53:46 -0300

    Added tables

--- ForgeSheets/utilities_app/views.py
+++ ForgeSheets/utilities_app/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def login(request):
   return render(request, 'login.html')
+
+def cadastro(request):
+  if request.method == 'POST':
+    form = CadastroForm(request.POST)
+    
