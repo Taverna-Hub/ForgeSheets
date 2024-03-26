246fed16b7afe02205b932c9e03fa1396fbf47fe
n3waz <lhk@cesar.school>
2024-03-26 18:51:14 -0300

    solved merge problem and added register function

--- ForgeSheets/utilities_app/views.py
+++ ForgeSheets/utilities_app/views.py
@@ -3,20 +3,34 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth import login as login_django
-from django.contrib.auth.decorations import login_required
+# from django.contrib.auth.decorations import login_required
 
-
-def cadastro(request):
-	if request.method == 'POST':
-		form = CadastroForm(request.POST)
 
 #precisa mudar o nome das html
-def home(request):
-	if request.user.is_autenticated:
-		name = request.user.username
-		return render (request, 'login.hmtl', {'singed_in': True, 'user':name}) 
-	else:
-		return render (request, 'login.html')
+# def home(request):
+# 	if request.user.is_autenticated:
+# 		name = request.user.username
+# 		return render (request, 'login.hmtl', {'singed_in': True, 'user':name}) 
+# 	else:
+# 		return render (request, 'login.html')
+	
+def cadastro(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('Já existe um usuário com esse username')
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save
+        cadastrado = True
+        return redirect('home')
+    else:
+        return render(request, 'cadastro.html')
 
 def login(request):
   	#return render(request, 'login.html')
