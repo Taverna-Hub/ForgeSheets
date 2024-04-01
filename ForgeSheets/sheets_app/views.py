ec3b1603a208311b22d65d355255e33615774bc3
kat0 <lhk@cesar.school>
2024-04-01 20:25:39 -0300

    position of class (sheets and createsheets) fixedabove all other classes

--- ForgeSheets/sheets_app/views.py
+++ ForgeSheets/sheets_app/views.py
@@ -5,6 +5,64 @@ from .models import Equipment, Sheet
 from .utils import equipment_check
 from django.contrib import messages
 
+class Sheets(View):
+    def get(self, request):
+        return render(request, 'sheets.html')
+    
+
+class CreateSheet(View):
+    def get(self, request):
+        return render(request, 'createsheets.html')
+    
+    def post(self, request):
+        name = request.POST.get('name')
+        image = request.POST.get('image')
+
+        race = request.POST.get('race')
+        role = request.POST.get('role')
+
+        strength = request.POST.get('strength')
+        intelligence = request.POST.get('intelligence')
+        wisdom = request.POST.get('wisdom')
+        charisma = request.POST.get('charisma')
+        constitution = request.POST.get('constitution')
+        speed = request.POST.get('speed')
+
+        healthPoint = request.POST.get('healthPoint')
+        healthPointMax = request.POST.get('healthPointMax')
+        mana = request.POST.get('mana')
+        manaMax = request.POST.get('manaMax')
+        exp = request.POST.get('exp')
+        expMax = request.POST.get('expMax')
+
+        notes = request.POST.get('notes')
+        description = request.POST.get('description')
+ 
+        user = request.POST.get('user')
+
+        sheet = Sheets(
+            name = name, 
+            image = image,
+            race = race,
+            role = role,
+            strength = strength,
+            intelligence = intelligence,
+            wisdom = wisdom,
+            charisma = charisma,
+            constitution = constitution,
+            speed = speed,
+            healthPoint = healthPoint,
+            healthPointMax = healthPointMax,
+            mana = mana,
+            manaMax = manaMax,
+            exp = exp,
+            expMax = expMax,
+            notes = notes,
+            description = description,
+            user = user,
+        )
+        sheet.save()
+        return HttpResponse('Ficha salva com sucesso!')
 class AddEquipmentView(View):
 
     # TO DO: Tratar se um equipamento já existe
@@ -115,67 +173,3 @@ class EditEquipmentView(View):
             equipment.save()
 
             return redirect('sheets:list_equipment')
-
-class Sheets(View):
-    def get(self, request):
-        return render(request, 'sheets.html')
-    
-
-class CreateSheet(View):
-    def get(self, request):
-        return render(request, 'createsheets.html')
-<<<<<<< HEAD
-    
-    def post(self, request):
-        name = request.POST.get('name')
-        image = request.POST.get('image')
-
-        race = request.POST.get('race')
-        role = request.POST.get('role')
-
-        strength = request.POST.get('strength')
-        intelligence = request.POST.get('intelligence')
-        wisdom = request.POST.get('wisdom')
-        charisma = request.POST.get('charisma')
-        constitution = request.POST.get('constitution')
-        speed = request.POST.get('speed')
-
-        healthPoint = request.POST.get('healthPoint')
-        healthPointMax = request.POST.get('healthPointMax')
-        mana = request.POST.get('mana')
-        manaMax = request.POST.get('manaMax')
-        exp = request.POST.get('exp')
-        expMax = request.POST.get('expMax')
-
-        notes = request.POST.get('notes')
-        description = request.POST.get('description')
- 
-        user = request.POST.get('user')
-
-        sheet = Sheets(
-            name = name, 
-            image = image,
-            race = race,
-            role = role,
-            strength = strength,
-            intelligence = intelligence,
-            wisdom = wisdom,
-            charisma = charisma,
-            constitution = constitution,
-            speed = speed,
-            healthPoint = healthPoint,
-            healthPointMax = healthPointMax,
-            mana = mana,
-            manaMax = manaMax,
-            exp = exp,
-            expMax = expMax,
-            notes = notes,
-            description = description,
-            user = user,
-        )
-        sheet.save()
-        return HttpResponse('Ficha salva com sucesso!')
-            
-=======
->>>>>>> 7897e5f2ad5a94e91c7bce59b901fe705ae76a38
-
