245dffa0cb8f0114fe852d8e38fec619d62d58c9
Ana <acgs@cesar.school>
2024-04-03 21:59:41 -0300

    staticfiles

--- ForgeSheets/ForgeSheets/settings.py
+++ ForgeSheets/ForgeSheets/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = os.environ.get('DJANGO_STATIC_URL', "/static/")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
 
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
