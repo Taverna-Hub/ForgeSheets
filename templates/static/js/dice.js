1e72ffd0a910789da25bf55d495b3b4302000311
viniciusdandrade <vaj@cesar.school>
2024-04-26 22:04:58 -0300

    stylizing dice

--- templates/static/js/dice.js
+++ templates/static/js/dice.js
@@ -6,44 +6,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const resultDisplay = document.getElementById('resultDisplay');
 
 
-    const modalHeader = document.querySelector('.modal-header');
 
    
-    function dragElement(element, header) {
-        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        if (header) {
-            
-            header.onmousedown = dragMouseDown;
-        } else {
-           
-            element.onmousedown = dragMouseDown;
-        }
-
-        function dragMouseDown(e) {
-            e.preventDefault();
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            document.onmouseup = closeDragElement;
-            document.onmousemove = elementDrag;
-        }
-
-        function elementDrag(e) {
-            e.preventDefault();
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            element.style.top = (element.offsetTop - pos2) + "px";
-            element.style.left = (element.offsetLeft - pos1) + "px";
-        }
-
-        function closeDragElement() {
-            document.onmouseup = null;
-            document.onmousemove = null;
-        }
-    }
-
-    dragElement(diceModal, modalHeader);
+    
 
     diceButton.addEventListener('click', function() {
         diceModal.style.display = "block";
@@ -56,8 +21,11 @@ document.addEventListener('DOMContentLoaded', function () {
     formDice.onsubmit = function(event) {
         event.preventDefault();
         const quantity = parseInt(document.getElementById('quantityDice').value) || 1;
-        const diceType = parseInt(document.getElementById('typeDice').value) || 6;
+        
+        const diceType = parseInt(document.getElementById('typeDice').value) || 6 ;
+        
         const modifier = parseInt(document.getElementById('modDice').value) || 0;
+        
         let rolls = [];
         let total = 0;
 
@@ -68,6 +36,46 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         total += modifier;
-        resultDisplay.innerHTML = `<strong>Resultado: </strong> ${total} <br> <strong>Roladas:</strong> ${rolls.join(', ')}`;
+        resultDisplay.innerHTML = `<strong>Total: </strong> ${total} <br> <strong>Rolagens:</strong> ${rolls.join(', ')}`;
     };
+
+    function dragElement(element) {
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+        element.onmousedown = dragMouseDown;
+
+        function dragMouseDown(e) {
+            
+            if (['INPUT', 'TEXTAREA', 'BUTTON', 'SELECT', 'OPTION'].includes(e.target.tagName)) {
+                return; 
+            }
+
+            e.preventDefault(); 
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e.preventDefault(); 
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            
+            element.style.top = (element.offsetTop - pos2) + "px";
+            element.style.left = (element.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+           
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+
+    
+    dragElement(diceModal);
+
 });
