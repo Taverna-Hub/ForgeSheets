8cc5cabfa5681c9bfe1b6a21c7a68f0cc1f8021e
viniciusdandrade <vaj@cesar.school>
2024-05-01 14:53:30 -0300

    Finished dice test

--- cypress/e2e/RollDice.cy.js
+++ cypress/e2e/RollDice.cy.js
@@ -36,4 +36,35 @@ describe('rolling dices', () => {
         cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "2d4")
         cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "2d6")
     })
+    it('quantity zero roll', () =>{
+        cy.visit('/');
+        cy.get('#loginzinho > form > #user').type('Gabriel')
+        cy.get('#loginzinho > form > #password').type('123')
+        cy.get('#loginzinho > form > button').click()
+
+        cy.get('.diceButton').click()
+        cy.get('#quantityDice').type('0 2')
+        cy.get('#typeDice').type('4 6')
+        cy.get('#modDice').type('3')
+        cy.get('.rollButton').click()
+        cy.get('#quantityError').invoke('text').should('have.string', "A quantidade de dados não pode ser zero.")
+
+        
+    })
+    it('non integer roll', () =>{
+        cy.visit('/');
+        cy.get('#loginzinho > form > #user').type('Gabriel')
+        cy.get('#loginzinho > form > #password').type('123')
+        cy.get('#loginzinho > form > button').click()
+
+        cy.get('.diceButton').click()
+        cy.get('#quantityDice').type('zero dois')
+        cy.get('#typeDice').type('vinte seis')
+        cy.get('#modDice').type('três')
+        cy.get('.rollButton').click()
+        cy.get('#quantityError').invoke('text').should('have.string', "A quantidade de dados deve ser composta apenas por números inteiros.")
+        cy.get('#typeError').invoke('text').should('have.string', "Os tipos dos dados devem ser composto apenas por números inteiros.")
+        cy.get('#modifierError').invoke('text').should('have.string', "Os modificadores devem ser compostos apenas por números inteiros.")
+        
+    })
 })
