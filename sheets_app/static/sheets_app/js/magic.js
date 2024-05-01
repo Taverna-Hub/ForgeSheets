a6473940666f60220c38129f94a0b2118a5bc1a3
Gustavo Mourato <gmam@cesar.school>
2024-04-30 23:27:41 -0300

    adding cypress tests for magic in sheet

--- sheets_app/static/sheets_app/js/magic.js
+++ sheets_app/static/sheets_app/js/magic.js
@@ -62,7 +62,7 @@ function handleLoadMagicList(magic) {
               <input type="hidden" name="mgcDiceQuant" value="${magic.dice_quantity}" />
               <input type="hidden" name="mgcAttribute" value="${magic.attribute}" />
           </div>
-          ${magic.name} | ${magic.element} 
+          ${magic.name} | ${magic.element}
           <button type="button" class="removeMagic" onclick="handleDeleteMagic(this)">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/></svg>
           </button>
