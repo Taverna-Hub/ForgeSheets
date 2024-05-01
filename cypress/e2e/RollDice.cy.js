7e116e7c9f020efa6834dbcdad0b8e185890f620
viniciusdandrade <vaj@cesar.school>
2024-04-30 23:02:53 -0300

    Fixing dice test

--- cypress/e2e/RollDice.cy.js
+++ cypress/e2e/RollDice.cy.js
@@ -1,8 +1,8 @@
 describe('rolling dices', () => {
     it('negative modifier roll', () =>{
         cy.visit('/');
-        cy.get('#loginzinho > form > #user').type('login1')
-        cy.get('#loginzinho > form > #password').type('login1')
+        cy.get('#loginzinho > form > #user').type('Gabriel')
+        cy.get('#loginzinho > form > #password').type('123')
         cy.get('#loginzinho > form > button').click()
 
         cy.get('.diceButton').click()
@@ -13,21 +13,23 @@ describe('rolling dices', () => {
 
         cy.get('#resultDisplay > :nth-child(1)').invoke('text').should('have.string', "Total:")
         cy.get('#resultDisplay > :nth-child(3)').invoke('text').should('have.string', "Detalhes:")
+        cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "-2")
     })
 
     it('positive modifier roll', () =>{
         cy.visit('/');
-        cy.get('#loginzinho > form > #user').type('login1')
-        cy.get('#loginzinho > form > #password').type('login1')
+        cy.get('#loginzinho > form > #user').type('Gabriel')
+        cy.get('#loginzinho > form > #password').type('123')
         cy.get('#loginzinho > form > button').click()
 
         cy.get('.diceButton').click()
         cy.get('#quantityDice').type('2 2')
         cy.get('#typeDice').type('4 6')
-        cy.get('#modDice').type('+3')
+        cy.get('#modDice').type('3')
         cy.get('.rollButton').click()
 
         cy.get('#resultDisplay > :nth-child(1)').invoke('text').should('have.string', "Total:")
         cy.get('#resultDisplay > :nth-child(3)').invoke('text').should('have.string', "Detalhes:")
+        cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "+3")
     })
 })
