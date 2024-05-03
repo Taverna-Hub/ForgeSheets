655b3d727b7b0f3c41a63759225b3521b5db7477
Ana <acgs@cesar.school>
2024-05-03 12:39:34 -0300

    changing delete sheet button

--- sheets_app/static/sheets_app/js/sheet.js
+++ sheets_app/static/sheets_app/js/sheet.js
@@ -13,22 +13,3 @@ function closeChooseSheetModal() {
 
 closeChooseSheetModalBtn?.addEventListener('click', () => closeChooseSheetModal());
 createSheetBtn?.addEventListener('click', () => openChooseSheetModal());
-
-//Modal de deletar ficha
-const deleteSheetModal = document.querySelector('.deleteSheetModal');
-const deleteSheetBtn = document.querySelectorAll('.deleteSheet');
-const closedeleteSheetModalBtn = document.querySelector('#closeDeleteSheet');
-
-
-function opendeleteSheetModal() {
-  deleteSheetModal.style.display = "flex";
-}
-
-function closedeleteSheetModal() {
-  deleteSheetModal.style.display = "none";
-}
-
-closedeleteSheetModalBtn?.addEventListener('click', () => closedeleteSheetModal());
-deleteSheetBtn.forEach(button => {
-  button?.addEventListener('click', () => opendeleteSheetModal());
-})
