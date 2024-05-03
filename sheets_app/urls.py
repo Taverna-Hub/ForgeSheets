49fd9233a76215827de01323fbd950eca0506642
sophia <deletrasopa@gmail.com>
2024-05-03 11:59:33 -0300

    adding delete equipment function on view sheet

--- sheets_app/urls.py
+++ sheets_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('criar/', views.CreateSheetView.as_view(), name="create_sheets"),
     path('criar/<slug:id>/', views.CreateSheetInCampaingView.as_view(), name="create_sheetsInCampaign"),
     path('visualizar/<int:id>/', views.EditSheetView.as_view(), name="edit_sheet"),
-    path('deletar_ficha/', views.DeleteSheetView.as_view(), name="delete_sheet")
+    path('deletar_ficha/', views.DeleteSheetView.as_view(), name="delete_sheet"),
+    path('deletar_equipamento/<int:id>', views.DelEquipmentView.as_view(), name="delete_equipment")
 ]
