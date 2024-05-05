f1d8e1511432fa9d185144e62f59184b2a6243cf
Gustavo Mourato <gmam@cesar.school>
2024-05-05 18:56:11 -0300

    finishing dice roll test

--- cypress/e2e/RollDice.cy.js
+++ cypress/e2e/RollDice.cy.js
@@ -1,71 +1,65 @@
-// describe('rolling dices', () => {
-//         it('succesful dice roll', () =>{
-//             cy.visit('/');
-//             cy.get('#loginzinho > form > #user').type('login1')
-//             cy.get('#loginzinho > form > #password').type('login1')
-//             cy.get('#loginzinho > form > button').click()
+describe('rolling dices', () => {
+    it('succesful dice roll', () =>{
+        cy.visit('/');
+        cy.get('#loginzinho > form > #user').type('login1')
+        cy.get('#loginzinho > form > #password').type('login1')
+        cy.get('#loginzinho > form > button').click()
     
-//             cy.get('.diceButton').click()
-//             cy.get('#quantityDice').type('2 3')
-//             cy.get('#typeDice').type('6 8')
-//             cy.get('#modDice').type('2')
-//             cy.get('.rollButton').click()
+        cy.get('.diceButton').click()
+        cy.get('#quantityDice').type('2 3')
+        cy.get('#typeDice').type('6 8')
+        cy.get('#modDice').type('2')
+        cy.get('.rollButton').click()
     
-//             cy.get('#resultDisplay > :nth-child(1)').invoke('text').should('have.string', "Total:")
-//             cy.get('#resultDisplay > :nth-child(3)').invoke('text').should('have.string', "Detalhes:")
-//             cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "+2")
-//             cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "2d6")
-//             cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "3d8")
-//         })
+        cy.get('#resultDisplay > :nth-child(1)').invoke('text').should('have.string', "Total:")
+        cy.get('#resultDisplay > :nth-child(3)').invoke('text').should('have.string', "Detalhes:")
+        cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "+2")
+        cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "2d6")
+        cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "3d8")
+    })
 
-    // it('negative modifier roll', () =>{
-    //     cy.visit('/');
-    //     cy.get('#loginzinho > form > #user').type('login1')
-    //     cy.get('#loginzinho > form > #password').type('login1')
-    //     cy.get('#loginzinho > form > button').click()
+    it('trying to submit an out-of-scope modifier', () =>{
+        cy.visit('/');
+        cy.get('#loginzinho > form > #user').type('login1')
+        cy.get('#loginzinho > form > #password').type('login1')
+        cy.get('#loginzinho > form > button').click()
 
-    //     cy.get('.diceButton').click()
-    //     cy.get('#quantityDice').type('2 1')
-    //     cy.get('#typeDice').type('10 20')
-    //     cy.get('#modDice').type('-2')
-    //     cy.get('.rollButton').click()
+        cy.get('.diceButton').click()
+        cy.get('#quantityDice').type('2 3')
+        cy.get('#typeDice').type('6 8')
+        cy.get('#modDice').type('-800')
+        cy.get('.rollButton').click()
 
-    //     cy.get('#resultDisplay > :nth-child(1)').invoke('text').should('have.string', "Total:")
-    //     cy.get('#resultDisplay > :nth-child(3)').invoke('text').should('have.string', "Detalhes:")
-    //     cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "-2")
-    //     cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "2d10")
-    //     cy.get('#resultDisplay > :nth-child(4)').invoke('text').should('have.string', "1d20")
-    // })
+        cy.get('#modifierError > span').invoke('text').should('have.string', 'Insira um número entre -20 e 20.')
+    })
 
-//     it('quantity zero roll', () =>{
-//         cy.visit('/');
-//         cy.get('#loginzinho > form > #user').type('Gabriel')
-//         cy.get('#loginzinho > form > #password').type('123')
-//         cy.get('#loginzinho > form > button').click()
+    it('trying to submit different quantities of types and dice', () =>{
+        cy.visit('/');
+        cy.get('#loginzinho > form > #user').type('login1')
+        cy.get('#loginzinho > form > #password').type('login1')
+        cy.get('#loginzinho > form > button').click()
 
-//         cy.get('.diceButton').click()
-//         cy.get('#quantityDice').type('0 2')
-//         cy.get('#typeDice').type('4 6')
-//         cy.get('#modDice').type('3')
-//         cy.get('.rollButton').click()
-//         cy.get('#quantityError').invoke('text').should('have.string', "A quantidade de dados não pode ser zero.")
+        cy.get('.diceButton').click()
+        cy.get('#quantityDice').type('2 3 6')
+        cy.get('#typeDice').type('6 8')
+        cy.get('#modDice').type('2')
+        cy.get('.rollButton').click()
 
-        
-//     })
-//     it('non integer roll', () =>{
-//         cy.visit('/');
-//         cy.get('#loginzinho > form > #user').type('Gabriel')
-//         cy.get('#loginzinho > form > #password').type('123')
-//         cy.get('#loginzinho > form > button').click()
+        cy.get('#quantityError > span').invoke('text').should('have.string', 'Adicione a quantidade de dados rolados para cada tipo de dado.')        
+    })
 
-//         cy.get('.diceButton').click()
-//         cy.get('#quantityDice').type('zero dois')
-//         cy.get('#typeDice').type('vinte seis')
-//         cy.get('#modDice').type('três')
-//         cy.get('.rollButton').click()
-//         cy.get('#quantityError').invoke('text').should('have.string', "A quantidade de dados deve ser composta apenas por números inteiros.")
-//         cy.get('#typeError').invoke('text').should('have.string', "Os tipos dos dados devem ser composto apenas por números inteiros.")
-//         cy.get('#modifierError').invoke('text').should('have.string', "Os modificadores devem ser compostos apenas por números inteiros.")
-        
-//     })
+    it('trying to submit ouf-of-scope dice type', () =>{
+        cy.visit('/');
+        cy.get('#loginzinho > form > #user').type('login1')
+        cy.get('#loginzinho > form > #password').type('login1')
+        cy.get('#loginzinho > form > button').click()
+
+        cy.get('.diceButton').click()
+        cy.get('#quantityDice').type('2 4 6')
+        cy.get('#typeDice').type('5 13 2')
+        cy.get('#modDice').type('2')
+        cy.get('.rollButton').click()
+
+        cy.get('#typeError > span').invoke('text').should('have.string', 'Somente D4, D6, D8, D10, D12, D20, e D100 são permitidos.')
+    })
 })
