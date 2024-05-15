1d7c435ef9d04aae551ecf3ad1415c9d6ae28c6c
sophia <deletrasopa@gmail.com>
2024-05-15 14:37:31 -0300

    adding users do django admin

--- utilities_app/admin.py
+++ utilities_app/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth import admin as admin_auth_django
+
+from utilities_app.models import Users
+
+@admin.register(Users)
+class UsersAdmin(admin_auth_django.UserAdmin):
+  model = Users
