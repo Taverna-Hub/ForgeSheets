7959d1ee1c1e101aa79f2937b51bbecbf0b79544
sophia <deletrasopa@gmail.com>
2024-05-30 15:51:05 -0300

    refactoring race css

--- campaigns_app/static/campaigns_app/js/races.js
+++ campaigns_app/static/campaigns_app/js/races.js
@@ -1,100 +1,90 @@
+// Adicionar raça
+const openAddRaceModalBtn = document.querySelector(".addRace");
+const cancelAddRaceModalBtn = document.querySelector("#cancelAddRace");
+const closeAddRaceModalBtn = document.querySelector(".closeAddModal");
+const addRaceModal = document.querySelector(".ModalCreateRace");
 
-    const openAddRaceModalBtn = document.querySelector(".addRace");
-    const cancelAddRaceModalBtn = document.querySelector("#cancelAddRace");
-    const addRaceModal = document.querySelector(".ModalCreateRace");
+function handleOpenAddRaceModal() {
+    addRaceModal.style.display = "flex";
+}
 
-    function handleOpenAddRaceModal() {
-        addRaceModal.style.display = "flex";
-    }
+function handleCloseAddRaceModal() {
+    addRaceModal.style.display = "none";
+}
 
-    function handleCloseAddRaceModal() {
-        addRaceModal.style.display = "none";
-    }
+openAddRaceModalBtn?.addEventListener("click", handleOpenAddRaceModal);
+cancelAddRaceModalBtn?.addEventListener("click", handleCloseAddRaceModal);
+closeAddRaceModalBtn?.addEventListener("click", handleCloseAddRaceModal);
 
-    if (openAddRaceModalBtn) {
-        openAddRaceModalBtn.addEventListener("click", handleOpenAddRaceModal);
-    }
 
-    if (cancelAddRaceModalBtn) {
-        cancelAddRaceModalBtn.addEventListener("click", handleCloseAddRaceModal);
-    }
+// Editar raça
+const editButtons = document.querySelectorAll('.editRace');
+const editModal = document.querySelector('#editRaceModal');
+const closeEditModalButton = document.querySelector('.closeEditModal');
+const cancelEditButton = document.querySelector('#cancelEdit');
 
-   
-    const editButtons = document.querySelectorAll('.editRace');
-    const editModal = document.getElementById('editRaceModal');
-    const closeEditModalButton = document.getElementById('closeEditModal');
-    const cancelEditButton = document.getElementById('cancelEdit');
+editButtons.forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        const raceId = this.dataset.id;
+        const raceName = this.dataset.name;
+        const strengthBuff = this.dataset.strength;
+        const intelligenceBuff = this.dataset.intelligence;
+        const wisdomBuff = this.dataset.wisdom;
+        const charismaBuff = this.dataset.charisma;
+        const constitutionBuff = this.dataset.constitution;
+        const speedBuff = this.dataset.speed;
 
-    editButtons.forEach(button => {
-        button.addEventListener('click', function(event) {
-            event.preventDefault();
-            const raceId = this.dataset.id;
-            const raceName = this.dataset.name;
-            const strengthBuff = this.dataset.strength;
-            const intelligenceBuff = this.dataset.intelligence;
-            const wisdomBuff = this.dataset.wisdom;
-            const charismaBuff = this.dataset.charisma;
-            const constitutionBuff = this.dataset.constitution;
-            const speedBuff = this.dataset.speed;
+        document.getElementById('edit_race_id').value = raceId;
+        document.getElementById('edit_name').value = raceName;
+        document.getElementById('edit_strength_buff').value = strengthBuff;
+        document.getElementById('edit_intelligence_buff').value = intelligenceBuff;
+        document.getElementById('edit_wisdom_buff').value = wisdomBuff;
+        document.getElementById('edit_charisma_buff').value = charismaBuff;
+        document.getElementById('edit_constitution_buff').value = constitutionBuff;
+        document.getElementById('edit_speed_buff').value = speedBuff;
 
-            document.getElementById('edit_race_id').value = raceId;
-            document.getElementById('edit_name').value = raceName;
-            document.getElementById('edit_strength_buff').value = strengthBuff;
-            document.getElementById('edit_intelligence_buff').value = intelligenceBuff;
-            document.getElementById('edit_wisdom_buff').value = wisdomBuff;
-            document.getElementById('edit_charisma_buff').value = charismaBuff;
-            document.getElementById('edit_constitution_buff').value = constitutionBuff;
-            document.getElementById('edit_speed_buff').value = speedBuff;
-
-            editModal.style.display = 'flex';
-        });
+        editModal.style.display = 'flex';
     });
+});
 
-    if (closeEditModalButton) {
-        closeEditModalButton.addEventListener('click', function() {
-            editModal.style.display = 'none';
-        });
-    }
+closeEditModalButton?.addEventListener('click', function() {
+    editModal.style.display = 'none';
+});
+cancelEditButton?.addEventListener('click', function() {
+    editModal.style.display = 'none';
+});
 
-    if (cancelEditButton) {
-        cancelEditButton.addEventListener('click', function() {
-            editModal.style.display = 'none';
-        });
-    }
+// Deletar raça
+const deleteButtons = document.querySelectorAll('.delete_race_button');
+const deleteModal = document.querySelector('#deleteRaceModal');
+const closeDeleteModalButton = document.querySelector('.closeDeleteModal');
+const cancelDeleteButton = document.querySelector('#cancelDelete');
+const deleteFormButton = document.querySelector('.delete_race');
 
-    const deleteButtons = document.querySelectorAll('.delete_race_button');
-    const deleteModal = document.getElementById('deleteRaceModal');
-    const closeDeleteModalButton = document.querySelector('.closeDeleteModal');
-    const cancelDeleteButton = document.getElementById('cancelDelete');
-    const deleteFormButton = document.querySelector('.delete_race');
-
-    deleteButtons.forEach(button => {
-        button.addEventListener('click', function(event) {
-            event.preventDefault();
-            const raceId = this.dataset.id;
-            document.getElementById('delete_race_id').value = raceId;
-            deleteModal.style.display = 'flex';
-        });
+deleteButtons.forEach(button => {
+    button.addEventListener('click', function(event) {
+        event.preventDefault();
+        const raceId = this.dataset.id;
+        document.querySelector('#delete_race_id').value = raceId;
+        deleteModal.style.display = 'flex';
     });
+});
 
-    if (closeDeleteModalButton) {
-        closeDeleteModalButton.addEventListener('click', function() {
-            deleteModal.style.display = 'none';
-        });
+closeDeleteModalButton?.addEventListener('click', function() {
+    deleteModal.style.display = 'none';
+});
+
+cancelDeleteButton?.addEventListener('click', function() {
+    deleteModal.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+    if (event.target === editModal) {
+        editModal.style.display = 'none';
     }
-
-    if (cancelDeleteButton) {
-        cancelDeleteButton.addEventListener('click', function() {
-            deleteModal.style.display = 'none';
-        });
+    if (event.target === deleteModal) {
+        deleteModal.style.display = 'none';
     }
-
-
-    window.addEventListener('click', function(event) {
-        if (event.target === editModal) {
-            editModal.style.display = 'none';
-        }
-        if (event.target === deleteModal) {
-            deleteModal.style.display = 'none';
-        }
-    });
+});
