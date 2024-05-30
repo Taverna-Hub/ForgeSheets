8f1f8f0e75f2d24cbd428fba61f75630a728d5b3
Gabriel Albuquerque <gabrielmca2004@gmail.com>
2024-05-29 23:52:56 -0300

    starting to style class edit modal

--- campaigns_app/static/campaigns_app/js/class.js
+++ campaigns_app/static/campaigns_app/js/class.js
@@ -12,5 +12,35 @@ function handleCloseAddClassModal() {
   classModal.style.display = "none";
 }
 
+    const editButtons = document.querySelectorAll('.editClass');
+    const editModal = document.getElementById('editClassModal');
+    const closeEditModalButton = document.getElementById('closeEditModal');
+    const cancelEditButton = document.getElementById('cancelEdit');
+
+editButtons.forEach(button => {
+    button.addEventListener('click', function(event){
+    const classId = this.dataset.id;
+    const className = this.dataset.name;
+    const classRole = this.dataset.role;
+
+    document.getElementById('edit_class_id').value = classId;
+    document.getElementById('edit_name').value = className;
+    document.getElementById('edit_role').value = classRole;
+
+    editModal.style.display = 'flex';
+    });
+    });
+
+    if (closeEditModalButton) {
+      closeEditModalButton.addEventListener('click', function() {
+          editModal.style.display = 'none';
+      });
+  }
+
+    if (cancelEditButton) {
+      cancelEditButton.addEventListener('click', function() {
+          editModal.style.display = 'none';
+      });
+  }
 openAddClassModalBtn.addEventListener("click", () => handleOpenAddClassModal());
 cancelAddClassModalBtn.addEventListener("click", () => handleCloseAddClassModal());
