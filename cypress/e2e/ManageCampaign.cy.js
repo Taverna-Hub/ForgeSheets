0bb6e4583a31ab1a5c9481aaffdf360f760c0a17
Gustavo Mourato <gmam@cesar.school>
2024-05-29 21:41:26 -0300

    finishing hp and mana campaign management test

--- cypress/e2e/ManageCampaign.cy.js
+++ cypress/e2e/ManageCampaign.cy.js
@@ -2,6 +2,32 @@ Cypress.Commands.add('deleteAllUsers', () => {
     cy.exec('python delete_users.py', { failOnNonZeroExit: false })
 });
 
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+  })
+
+Cypress.Commands.add('createCampaignRaceClass', () => {
+    cy.get('#manageRaces').click()
+    cy.get('.addRace').click()
+    cy.get('#name').type('Humano')
+    cy.get('#strength_buff').type('1')
+    cy.get('#intelligence_buff').type('1')
+    cy.get('#wisdom_buff').type('1')
+    cy.get('#charisma_buff').type('1')
+    cy.get('#constitution_buff').type('1')
+    cy.get('#speed_buff').type('1')
+    cy.get('#submit_button').click()
+    
+    cy.get('.back').click()
+
+    cy.get('#manageClasses').click()
+    cy.get('.addClass').click()
+    cy.get('.classNameContainer > input').type('Guerreiro')
+    cy.get('#physicalDamage').check()
+    cy.get('.actions > [type="submit"]').click()
+    cy.get('.back').click()
+});
+
 Cypress.Commands.add('createCampaignSheet', () => {
     cy.get('.code').invoke('text').then((text) => {
         const copiedText = text;
@@ -12,18 +38,25 @@ Cypress.Commands.add('createCampaignSheet', () => {
         
     })
     cy.get('.sheetHeader > :nth-child(1) > input').type('Roronoa Zoro')
-    cy.get('#intelligence').type('10')
-    cy.get('#wisdom').type('10')
-    cy.get('#charisma').type('10')
-    cy.get('#constitution').type('10')
-    cy.get('#speed').type('10')
+    cy.get('#race').select('Humano')
+    cy.get('#class').select('Guerreiro')
+    cy.get('#strength').type('1')
+    cy.get('#intelligence').type('1')
+    cy.get('#wisdom').type('1')
+    cy.get('#charisma').type('1')
+    cy.get('#constitution').type('1')
+    cy.get('#speed').type('1')
     cy.get('.openImageBtn').click()
     cy.get('.image').type('https://i.pinimg.com/originals/9d/da/91/9dda91ebc1300a11f8d8ad05bbdbf92e.jpg')
     cy.get('#addImageBtn').click()
-    cy.get('#healthPointMax').type('200')
-    cy.get('#manaMax').type('50')
-    cy.get('#exp').type('50')
+    cy.get('#healthPointMax').type('100')
+    cy.get('#manaMax').type('100')
+    cy.get('#exp').type('0')
+    cy.get('#description').type('Gosto de espada')
     cy.get('#submit_button').click()
+
+    cy.get('.campaign > a').click()
+    cy.get('.campaignCard > :nth-child(1)').click()
 });
 
 Cypress.Commands.add('registerLoginCreateCampaign', () => {
@@ -97,6 +130,35 @@ describe('manage campaign tests', () => {
         cy.exec('python manage.py migrate')
         cy.deleteAllUsers();
         cy.registerLoginCreateCampaign()
+        cy.createCampaignRaceClass()
         cy.createCampaignSheet()
+
+        cy.get('.sheetCard > #Card').click()
+        cy.get('#healthPointMax').click()
+        cy.get('#healthPointMax').clear()
+        cy.get('#healthPointMax').type('200')
+        cy.get('#healthPointMax').click()
+
+        cy.get('#healthPoint').click()
+        cy.get('#healthPoint').clear()
+        cy.get('#healthPoint').type('150')
+        cy.get('#healthPoint').click()      
+
+        cy.get('#manaMax').click()
+        cy.get('#manaMax').clear()
+        cy.get('#manaMax').type('150')
+        cy.get('#manaMax').click()
+
+        cy.get('#mana').click()
+        cy.get('#mana').clear()
+        cy.get('#mana').type('75')
+        cy.get('#mana').click()
+        cy.get('#submit_button').click()
+
+        cy.get('input[name="healthPointMax"]').should('have.value', "200")
+        cy.get('input[name="healthPoint"]').should('have.value', "150")
+        cy.get('input[name="manaMax"]').should('have.value', "150")
+        cy.get('input[name="manaActual"]').should('have.value', "75")
+
     })
 })
